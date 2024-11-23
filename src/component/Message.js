2f6a47ec918a5f39625fdf154219a7d4b6306c54
Hugo Arnaudeau <hugo.arnaudeau@gmail.com>
2024-11-23 16:24:03 +0100

    removeMethod

--- src/component/Message.js
+++ src/component/Message.js
@@ -63,15 +63,66 @@ function Message() {
           {/* Liste déroulante pour sélectionner un utilisateur */}
           <select value={selectedUser} onChange={handleChange}>
             {options.map((option) => (
-              <option key={option.value} value={option.value}>
+              <option value={option.value}>
                 {option.label}
               </option>
             ))}
           </select>
+          {/* Chargement des messages */}
+          {isLoading? (
+            <p>Chargement des messages </p>
+          ) : (
+            <ul>
+          {messages.map((message) => (
+            <li key={message.id}>
+              {message.text}{" "}
+            </li>
+          ))}
+          </ul> 
+        )}
         </div>
       </div>
     </div>
   );
 }
+{/*
+    return (
+    <div>
+      <h2>Messages</h2>
 
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Liste des messages</h5>
+          <select>
+          {options.map((options) => (
+              <options value= {options.value}>{options.label}</options>
+          ))}
+          </select>
+          {Bouton pour récupérer les messages }
+          <button onClick={fetchMessages} className="btn btn-primary mb-3">
+            Charger les messages
+          </button>
+
+          Affichage du statut de chargement
+          {isLoading && <p>Chargement des messages...</p>}
+
+          <h5>Messages :</h5>
+          <ul>
+            {messages.length > 0 ? (
+              messages.map(msg => (
+                <li key={msg.id_message}>
+                  <strong>{msg.message_object} :</strong> {msg.message_body}
+                </li>
+              ))
+            ) : (
+              <p>Aucun message à afficher.</p>
+            )}
+            <button onClick={removeMessage} className='btn btn-primary mb-3'>Filtrer</button>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}}
+*/}
 export default Message;
